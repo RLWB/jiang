9c1ee9ba0cae3ca62651a0f0bb84526f9b2601bf
shenggj@glorysofter.com <shenggj@glorysofter.com>
2022-03-24 17:02:55 +0800

    移除配置文件中target字段

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,68 +2,75 @@
  * @Author: Kuntey
  * @Date: 2022-03-23 11:56:00
  * @LastEditors: Kuntey
- * @LastEditTime: 2022-03-23 12:01:52
+ * @LastEditTime: 2022-03-24 14:42:28
  * @Description:
  */
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+    // Target: https://go.nuxtjs.dev/config-target
+    // target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'nuxt-app',
-    htmlAttrs: {
-      lang: 'en'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'nuxt-app',
+        htmlAttrs: {
+            lang: 'en'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+        '@/assets/style/index.scss',
+        'element-ui/lib/theme-chalk/index.css',
+        '@/assets/variable.scss'
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+        '@/plugins/main'
+    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/element-ui'
-  ],
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: [
+        // Equivalent to { path: '~/components' }
+        '~/components',
+        { path: '~/components/business', extensions: ['vue'] },
+        { path: '~/components/common', extensions: ['vue'] }
+    ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+    ],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+    ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        transpile: [/^element-ui/],
+    },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    transpile: [/^element-ui/],
-  },
-
-  /*
-  ** Router configuration
-  */
-  router: {
-    // middleware: ['auth', 'i18n'],
-    extendRoutes (routes, resolve) {
-      routes.push({
-        path: '/',
-        redirect: {
-          name: '/lanhu_yinyuerenchuangzuozhongxinzuopinguanliyinyueshenhetongguo'
+    /*
+    ** Router configuration
+    */
+    router: {
+        // middleware: ['auth', 'i18n'],
+        extendRoutes (routes, resolve) {
+        routes.push({
+            path: '/',
+            redirect: {
+                name: '/lanhu_yinyuerenchuangzuozhongxinzuopinguanliyinyueshenhetongguo'
+            }
+        })
         }
-      })
-    }
-  },
+    },
 }
