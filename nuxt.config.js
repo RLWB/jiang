6f3eaacff1c55fc981adc9306d6766b5eaf36cd2
rlwb <rlwb1017669154@gmail.com>
2022-04-21 10:13:49 +0800

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,94 +6,100 @@
  * @Description:
  */
 export default {
-    // Target: https://go.nuxtjs.dev/config-target
-    // target: 'static',
-    server: {
-        port: 3009,
-        host: '127.0.0.1'
+  // Target: https://go.nuxtjs.dev/config-target
+  // target: 'static',
+  server: {
+    port: 3009,
+    host: "127.0.0.1",
+  },
+  env: {
+    baseUrl: process.env.BASE_URL,
+  },
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: "nuxt-app",
+    htmlAttrs: {
+      lang: "en",
     },
-    env: {
-        baseUrl: process.env.BASE_URL
-    },
-    // Global page headers: https://go.nuxtjs.dev/config-head
-    head: {
-        title: 'nuxt-app',
-        htmlAttrs: {
-            lang: 'en'
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    // 'element-ui/lib/theme-chalk/index.css',
+    "@/assets/style/index.scss",
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: ["@/plugins/main"],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: [
+    // Equivalent to { path: '~/components' }
+    "~/components",
+    { path: "~/components/business", extensions: ["vue"] },
+    { path: "~/components/common", extensions: ["vue"] },
+  ],
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    [
+      "@nuxtjs/netlify-files",
+      {
+        /* module options */
+        netlifyToml: {
+          from: "/api",
+          to: "https://yinjiuyinyue-service.grizzlysoft.cn",
+          status: 200,
+          force: true,
         },
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
-            { name: 'format-detection', content: 'telephone=no' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
-
-    // Global CSS: https://go.nuxtjs.dev/config-css
-    css: [
-        // 'element-ui/lib/theme-chalk/index.css',
-        '@/assets/style/index.scss',
+      },
     ],
+  ],
 
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [
-        '@/plugins/main'
-    ],
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: ["@nuxtjs/axios"],
 
-    // Auto import components: https://go.nuxtjs.dev/config-components
-    components: [
-        // Equivalent to { path: '~/components' }
-        '~/components',
-        { path: '~/components/business', extensions: ['vue'] },
-        { path: '~/components/common', extensions: ['vue'] }
-    ],
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: [/^element-ui/],
+  },
 
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-    buildModules: [
-    ],
-
-    // Modules: https://go.nuxtjs.dev/config-modules
-    modules: [
-        '@nuxtjs/axios'
-    ],
-
-    // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {
-        transpile: [/^element-ui/],
+  /*
+   ** Router configuration
+   */
+  router: {
+    // middleware: ['auth', 'i18n'],
+    extendRoutes(routes, resolve) {
+      routes.push({
+        path: "/",
+        // redirect: {
+        //     name: '/lanhu_yinyuerenchuangzuozhongxinzuopinguanliyinyueshenhetongguo'
+        // }
+      });
     },
+  },
+  vue: {
+    config: {
+      productionTip: true,
+      devtools: true,
+    },
+  },
 
-    /*
-    ** Router configuration
-    */
-    router: {
-        // middleware: ['auth', 'i18n'],
-        extendRoutes (routes, resolve) {
-            routes.push({
-                path: '/',
-                // redirect: {
-                //     name: '/lanhu_yinyuerenchuangzuozhongxinzuopinguanliyinyueshenhetongguo'
-                // }
-            })
-        }
+  axios: {
+    proxy: process.env.NODE_ENV === "development" ? true : false,
+  },
+  proxy: {
+    [process.env.BASE_URL]: {
+      target: "https://yinjiuyinyue-service.grizzlysoft.cn/api/",
+      changeOrigin: true, // 是否跨域
+      pathRewrite: { ["^" + process.env.BASE_URL]: "" },
     },
-    vue: {
-        config: {
-            productionTip: true,
-            devtools: true
-        }
-    },
-
-    axios: {
-        proxy: process.env.NODE_ENV === 'development' ? true : false,
-    },
-    proxy: {
-        [process.env.BASE_URL]: {
-            target: 'https://yinjiuyinyue-service.grizzlysoft.cn/api/',
-            changeOrigin: true, // 是否跨域
-            pathRewrite: {['^' + process.env.BASE_URL] : ''}
-        }
-    }
-}
+  },
+};
